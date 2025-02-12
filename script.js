cfab77665a700b612b760937a2b520d4fc269212
Your Name <your-email@example.com>
2025-02-12 22:09:15 +0400

    Show quote and auther separately and modified quotes array of strings to array of object

--- script.js
+++ script.js
@@ -1,26 +1,58 @@
 const quotes = [
-  "The only way to do great work is to love what you do.",
-  "The future belongs to those who believe in the beauty of their dreams.",
-  "Believe you can and you're halfway there.",
-  "The best way to predict the future is to invent it.",
-  "The only limit to our realization of tomorrow is our doubts of today.",
-  "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.",
-  "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.",
-  "The only way to do great work is to love what you do.",
-  "In the middle of every difficulty lies opportunity.",
-  "The future belongs to those who believe in the beauty of their dreams.",
-  "Believe you can and you're halfway there.",
-  "The best way to predict the future is to invent it.",
+  {
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  },
+  {
+    quote: "The future belongs to those who believe in the beauty of their dreams.",
+    author: "Eleanor Roosevelt",
+  },
+  {
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    quote: "The best way to predict the future is to invent it.",
+    author: "Alan Kay",
+  },
+  {
+    quote: "The only limit to our realization of tomorrow is our doubts of today.",
+    author: "Franklin D. Roosevelt",
+  },
+  {
+    quote: "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    author: "Winston Churchill",
+  },
+  {
+    quote: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon",
+  },
+  {
+    quote: "In the middle of difficulty lies opportunity.",
+    author: "Albert Einstein",
+  },
+  {
+    quote: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Disney",
+  },
+  {
+    quote: "Everything you've ever wanted is on the other side of fear.",
+    author: "George Addair",
+  }
 ];
+
 const quoteElement = document.getElementById("quote");
+const quoteAuthorElement = document.getElementById("quote-author");
 const generateBtn = document.getElementById("generate-btn");
+
+
 
 function generateRandomQuote() {
   const randomIndex = Math.floor(Math.random() * quotes.length);
   const randomQuote = quotes[randomIndex];
-  quoteElement.textContent = randomQuote;
+  const { quote, author: qouteAuther } = randomQuote;
+  quoteElement.textContent = quote;
+  quoteAuthorElement.textContent = qouteAuther;
 }
 
 generateBtn.addEventListener("click", generateRandomQuote);
-
-generateRandomQuote();
