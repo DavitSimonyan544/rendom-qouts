fba400f5614a5ba068d6986b0e4617b1c9338533
Your Name <your-email@example.com>
2025-02-15 17:45:12 +0400

    Show favorite qoutes below current quote

--- qoutes.js
+++ qoutes.js
@@ -15,28 +15,34 @@ const quotes = [
   {
     quote: "The best way to predict the future is to invent it.",
     author: "Alan Kay",
+    isFavorite: false,
   },
   {
     quote:
       "The only limit to our realization of tomorrow is our doubts of today.",
     author: "Franklin D. Roosevelt",
+    isFavorite: false,
   },
   {
     quote:
       "Success is not final, failure is not fatal: it is the courage to continue that counts.",
     author: "Winston Churchill",
+    isFavorite: false,
   },
   {
     quote: "Life is what happens when you're busy making other plans.",
     author: "John Lennon",
+    isFavorite: false,
   },
   {
     quote: "In the middle of difficulty lies opportunity.",
     author: "Albert Einstein",
+    isFavorite: false,
   },
   {
     quote: "The way to get started is to quit talking and begin doing.",
     author: "Walt Disney",
+    isFavorite: false,
   },
   {
     quote: "Everything you've ever wanted is on the other side of fear.",
--- script.js
+++ script.js
@@ -1,16 +1,51 @@
 import quotes from "./qoutes.js";
 
-
 const quoteElement = document.getElementById("quote");
 const quoteAuthorElement = document.getElementById("quote-author");
 const generateBtn = document.getElementById("generate-btn");
+const toggleFavoriteBtn = document.getElementById("toggle-favorite-btn");
+const favoriteContainer = document.getElementById("favorites-container");
+let currentQuoteIndex;
 
 function generateRandomQuote() {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[randomIndex];
+  currentQuoteIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[currentQuoteIndex];
   const { quote, author } = randomQuote;
   quoteElement.textContent = quote;
   quoteAuthorElement.textContent = author;
+  toggleFavoriteBtn.textContent = randomQuote.isFavorite
+    ? "Remove from Favorites"
+    : "Add to Favorite";
+
+  toggleFavoriteBtn.style.display = "inline-block";
+}
+
+function toggleFavorite() {
+  const currentQuote = quotes[currentQuoteIndex];
+  currentQuote.isFavorite = !currentQuote.isFavorite;
+  toggleFavoriteBtn.textContent = currentQuote.isFavorite
+    ? "Remove from Favorites"
+    : "Add to Favorite";
+
+  if (currentQuote.isFavorite) {
+    const favoriteCard = document.createElement("div");
+    favoriteCard.classList.add("favorite-card");
+    favoriteCard.innerHTML = `
+    <p>${currentQuote.quote}</p>
+    <p class="author">${currentQuote.author}</p>
+    `;
+    favoriteContainer.appendChild(favoriteCard);
+  } else {
+    const favoritesCards = document.querySelectorAll(".favorite-card");
+    favoritesCards.forEach((card) => {
+      if (card.textContent.includes(currentQuote.quote)) {
+        card.remove();
+      }
+    });
+  }
 }
 
 generateBtn.addEventListener("click", generateRandomQuote);
+toggleFavoriteBtn.addEventListener("click", toggleFavorite);
+
+generateRandomQuote();
